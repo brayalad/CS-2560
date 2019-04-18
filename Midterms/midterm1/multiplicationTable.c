2668dbe2a33cdf2e6ecfaa4c3bc728d9d10b80e2
Bryan Ayala <brayalad@yahoo.com>
2019-04-18 14:46:07 -0700

    Added midterm2 folder

--- Midterms/midterm1/multiplicationTable.c
+++ Midterms/midterm1/multiplicationTable.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-
 
     printf("Enter size of table: ");
     int size;
     scanf("%d",&size);
 
-    int table[size][size];
+    int *table = (int *)malloc(size*size*sizeof(int));
 
     for(int i=0; i<size; ++i)
         for(int j=0; j<size; ++j)
-            table[i][j] = (i+1)*(j+1);
+            *(table + i*size + j) = (i+1)*(j+1);
 
     for(int i=0; i<size; ++i) {
         for (int j = 0; j < size; ++j)
-            printf("%d ", table[i][j]);
+            printf("%d ", *(table + i*size + j));
         printf("\n");
     }
 
